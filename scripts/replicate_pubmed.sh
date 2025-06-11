837435281b3a80d188c11866c74e5d317f69c6a3
Selin Ceydeli <sceydeli@login03.delftblue.tudelft.nl>
2025-06-11 15:14:02 +0200

    exp results for runtime test is added for pubmed

--- scripts/replicate_pubmed.sh
+++ scripts/replicate_pubmed.sh
@@ -1,2 +1,2 @@
 # Pubmed
-python src/run.py --data_name pubmed --lr 1e-3  --gnn-layers 1 --dim 128 --batch-size 1024  --epochs 3 --eps 1e-5 --gnn-drop 0.3 --dropout 0.3 --pred-drop 0.3 --att-drop 0.3  --num-heads 1  --thresh-1hop 1e-2 --thresh-non1hop 1e-2 --mask-input  --feat-drop 0.3 --l2 1e-4 --eval_steps 1 --decay 1 --non-verbose --runs 1 --device 0 > citeseer_pubmed_custom.log 2>&1
+python src/run.py --data_name pubmed --lr 1e-3  --gnn-layers 1 --dim 128 --batch-size 1024  --epochs 3 --eps 1e-5 --gnn-drop 0.3 --dropout 0.3 --pred-drop 0.3 --att-drop 0.3  --num-heads 1  --thresh-1hop 1e-2 --thresh-non1hop 1e-2 --mask-input  --feat-drop 0.3 --l2 1e-4 --eval_steps 1 --decay 1 --non-verbose --runs 1 --device 0 > pubmed_training_custom.log 2>&1
